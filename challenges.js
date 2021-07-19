b1b34f4f2bef0b86ae353adc55e9d1f8a6347b70
Alex <alexfrancisaaron@gmail.com>
2021-07-19 15:37:08 -0500

    completed longestPalindrome function

--- challenges.js
+++ challenges.js
@@ -1,9 +1,44 @@
-// Longest Palindromic Substring //
+///////////// Longest Palindromic Substring /////////////
 
 let sampleStr = "babad";
 
 function longestPalindrome(s) {
-    return sampleStr;
+    let palArray = createPalindromeArr(s);
+    let length = palArray[0].length;
+    let longestPal = palArray[0];
+    for (const element of palArray) {
+        if (element.length > length) {
+            longestPal = element;
+        }
+    }
+    return longestPal;
 }
 
-console.log(longestPalindrome(smapleStr));
+// HELPER FUNCTIONS //
+
+function createPalindromeArr(s) {
+    let palArr = [];
+    for (let i = 0; i < s.length; i++) {
+        let a = i;
+        let z = s.length - 1;
+        while (z > 0) {
+            console.log("a: " + s[a] + " z: " + s[z]);
+            if (s[a] === s[z]) {
+                let pal = s.slice(a, z + 1);
+                console.log(pal);
+                let reversedPal = pal.split("").reverse().join("");
+                if (pal === reversedPal) {
+                    palArr.push(pal);
+                }
+                z--;
+            } else {
+                z--;
+            }
+        }
+    }
+    return palArr;
+}
+
+console.log(longestPalindrome(sampleStr));
+
+// ITERATION //
