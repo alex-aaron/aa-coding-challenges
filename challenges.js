558f418dfc2091339fd7f09ca3cfe723a7abaf40
Alex <alexfrancisaaron@gmail.com>
2021-07-20 13:23:57 -0500

    completed isPalindrome function

--- challenges.js
+++ challenges.js
@@ -1,49 +1,13 @@
-///////////// Longest Palindromic Substring /////////////
+let sampleInt = 121;
 
-let sampleStr = "a";
-
-function longestPalindrome(s) {
-    let longestPal;
-    if (s.length === 1) {
-        longestPal = s;
+function isPalindrome(x) {
+    let reversedStr = x.toString().split("").reverse().join("");
+    let reversedInt = parseInt(reversedStr);
+    if (x === reversedInt) {
+        return true;
     } else {
-        let palArray = createPalindromeArr(s);
-    let length = palArray[0].length;
-    let longestPal = palArray[0];
-    for (const element of palArray) {
-        if (element.length > length) {
-            longestPal = element;
-        }
+        return false;
     }
-    }
-    return longestPal;
 }
 
-// HELPER FUNCTIONS //
-
-function createPalindromeArr(s) {
-    let palArr = [];
-    for (let i = 0; i < s.length; i++) {
-        let a = i;
-        let z = s.length - 1;
-        while (z > 0) {
-            console.log("a: " + s[a] + " z: " + s[z]);
-            if (s[a] === s[z]) {
-                let pal = s.slice(a, z + 1);
-                console.log(pal);
-                let reversedPal = pal.split("").reverse().join("");
-                if (pal === reversedPal) {
-                    palArr.push(pal);
-                }
-                z--;
-            } else {
-                z--;
-            }
-        }
-    }
-    return palArr;
-}
-
-console.log(longestPalindrome(sampleStr));
-
-// ITERATION //
+console.log(isPalindrome(sampleInt));
