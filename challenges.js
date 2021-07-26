5cbcb65bf992ee0c5fbd3330894a680b1e91e1e4
Alex <alexfrancisaaron@gmail.com>
2021-07-26 14:39:37 -0500

    added conditional logic to check if strings each contain the first letter

--- challenges.js
+++ challenges.js
@@ -1,9 +1,19 @@
 let sampleStr = ["flow", "flower", "flight"];
 
 function longestCommonPrefix(arr) {
-    let minPrefix = false;
+    let minPrefix = true;
     let comparisonStr = arr[0];
-    
+    for (const element of arr) {
+        if (element[0] !== comparisonStr[0]) {
+            minPrefix = false;
+        }
+    }
+    if (minPrefix === true) {
+        for (let i = 1; i < arr.length; i++) {
+            if ()
+        }
+    }
+ 
 }
 
 console.log(longestCommonPrefix(sampleStr));
