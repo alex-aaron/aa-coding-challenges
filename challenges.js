a9d97afdcae8529f335600cd993aa461434c956f
Alex <alexfrancisaaron@gmail.com>
2021-07-26 16:40:25 -0500

    revised longestCommonPrefix function

--- challenges.js
+++ challenges.js
@@ -2,16 +2,16 @@ let sampleStr = ["flow", "flower", "flight"];
 
 function longestCommonPrefix(arr) {
     let minPrefix = true;
+    let outputStr = "";
     let comparisonStr = arr[0];
     for (const element of arr) {
         if (element[0] !== comparisonStr[0]) {
             minPrefix = false;
         }
+
     }
     if (minPrefix === true) {
-        for (let i = 1; i < arr.length; i++) {
-            if ()
-        }
+        
     }
  
 }
