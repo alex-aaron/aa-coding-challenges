937ac5b5f02aa49c621b465e6df4f3b04ab605ee
Alex <alexfrancisaaron@gmail.com>
2021-07-27 18:54:45 -0500

    revised concat function

--- challenges.js
+++ challenges.js
@@ -3,15 +3,15 @@ function concat(...args) {
     let concatArr = [];
     for (let i = 0; i < args.length; i++) {
         if (args[i].length > 1) {
-            let subArr = element;
+            let subArr = args[i];
             for (let j = 0; j < subArr.length; j++) {
                 concatArr.push(subArr[j]);
             }
         } else {
-            concatArr.push(args[i]);
+            concatArr.push(args[i][0]);
         }
     }
     return concatArr;
 }
 
-console.log(concat([1], [2], [3], [4]));
+console.log(concat(['a'], ['b', 'c']));
