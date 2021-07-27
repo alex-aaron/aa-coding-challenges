116d843f4fca862523e9ad7ac4425ee3a6919ba5
Alex <alexfrancisaaron@gmail.com>
2021-07-27 18:57:10 -0500

    created derivative function

--- challenges.js
+++ challenges.js
@@ -1,17 +1,4 @@
 
-function concat(...args) {
-    let concatArr = [];
-    for (let i = 0; i < args.length; i++) {
-        if (args[i].length > 1) {
-            let subArr = args[i];
-            for (let j = 0; j < subArr.length; j++) {
-                concatArr.push(subArr[j]);
-            }
-        } else {
-            concatArr.push(args[i][0]);
-        }
-    }
-    return concatArr;
+function derivative(b, m) {
+    return Math.pow(b, m);
 }
-
-console.log(concat(['a'], ['b', 'c']));
