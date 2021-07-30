b6bfda6b26556d43a60398e5657d0156ef47043d
Alex <alexfrancisaaron@gmail.com>
2021-07-29 21:25:39 -0500

    revised typeof === array[index] logic

--- challenges.js
+++ challenges.js
@@ -7,8 +7,9 @@ function getLength(a) {
         return 0;
     }
     for (let i = 0; i < a.length; i++) {
-        if (typeof a[i] === 'object')
-        return true;
+        if (typeof a[i] !== 'object') {
+            nonNestedElements += 1;
+        }
     }
 }
 
