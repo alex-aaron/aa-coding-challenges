7a4da99f216db5bc65305b1cea144f15dc998db4
Alex <alexfrancisaaron@gmail.com>
2021-07-29 21:12:27 -0500

    tested typeof array[index] === object logic

--- challenges.js
+++ challenges.js
@@ -1,8 +1,15 @@
 
-let sampleInput = ;
+let sampleInput = [1, [2, 3]];
 
 function getLength(a) {
-    return a;
+    let nonNestedElements;
+    if (a.length === 0) {
+        return 0;
+    }
+    for (let i = 0; i < a.length; i++) {
+        if (typeof a[i] === 'object')
+        return true;
+    }
 }
 
 console.log(getLength(sampleInput));
