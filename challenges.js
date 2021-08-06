e59653d8592010fdff21d7ca66737d6ea21822df
Alex <alexfrancisaaron@gmail.com>
2021-08-06 13:02:41 -0500

    added console log statements to test

--- challenges.js
+++ challenges.js
@@ -2,15 +2,20 @@ let sampleArr = [1, 2, 3, 4, 5];
 
 // VANILLA JS ITERATION //
 
+console.log("Vanilla JS below:")
+
 for (var i = 0; i < sampleArray.length; i++) {
     console.log(sampleArr[i]);
 }
 
 // ES6 Iteration //
 
+console.log("ES6 Iteration");
+
 for (const element of sampleArr) {
     console.log(element);
 }
+
 
 
 
