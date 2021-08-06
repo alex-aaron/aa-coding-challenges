e75b3e178fa5c699b8b253ac1e40d50f6c7951a3
Alex <alexfrancisaaron@gmail.com>
2021-08-06 12:51:50 -0500

    example of vanilla js iteration

--- challenges.js
+++ challenges.js
@@ -1,21 +1,7 @@
+let sampleArr = [1, 2, 3, 4, 5];
 
-let sampleArr = [1, -1, 5, 3, -7, 4, 5, 6, -100, 6];
+// VANILLA JS ITERATION //
 
-function longestSubarraySum(a) {
-    let currentMax = a[0];
-    let maxSumsArr = [];
-    maxSumsArr.push(currentMax);
-    for (let i = 1; i < a.length; i++) {
-        currentMax = Math.max(a[i], currentMax + a[i]);
-        maxSumsArr.push(currentMax);
-    }
-    let maxSum = maxSumsArr[0];
-    for (const element of maxSumsArr) {
-        if (element > maxSum) {
-            maxSum = element;
-        }
-    }
-    return maxSum;
+for (var i = 0; i < sampleArray.length; i++) {
+    console.log(sampleArr[i]);
 }
-
-console.log(longestSubarraySum(a));
